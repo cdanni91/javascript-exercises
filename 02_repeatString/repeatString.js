521252a28bbcf2cebb7dc734cb64b9591a93654a
cdanni91 <cdanni91@gmail.com>
2025-04-05 15:49:02 -0300

    Finish exercise 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,26 @@
-const repeatString = function() {
 
-};
+
+const repeatString = function(string, num) {
+
+    let word = '';
+
+    if (num < 0) {
+        return 'ERROR';
+    } else {
+        for (let i = 0; i < num; i++) {
+            word = word + string ;
+        }
+    }
+
+    return word;
+
+    };
+
+randomNumber = Math.floor((Math.random()*1000));
+
+repeatString('', randomNumber);
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
