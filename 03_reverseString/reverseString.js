6f6b777c3880aece76703332f1ae62443beb04f6
cdanni91 <cdanni91@gmail.com>
2025-04-07 07:55:10 -0300

    Complete exercise 03 and OK all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    const arrayWord = word.split("");
+    let reverseWord = [];
 
+    for (let i = arrayWord.length - 1; i >= 0; i--) {
+        reverseWord.push(arrayWord[i]);
+    }
+
+    reverseWord = reverseWord.join("");
+
+    return reverseWord;
 };
+
+console.log(reverseString('hello there')); // Output: ["e", "r", "e", "h", " ", "o", "l", "l", "e", "h"]
+
 
 // Do not edit below this line
 module.exports = reverseString;
