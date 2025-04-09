14c5390a581281637129e4ebd870c08adaae9d47
cdanni91 <cdanni91@gmail.com>
2025-04-09 08:07:03 -0300

    Finished exercise 10, fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,48 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+
+    let a = 0;
+    let b = 1;
+    let c = a + b;
+    let result = 0;
+
+
+    if (number < 0) {
+
+        return 'OOPS';
+
+    } else if (typeof(number) === 'string') {
+        Number(number);
+    }
+
+    for (let i = 0; i < number; i++) {
+
+        if(i !== 0) {
+
+            result = a + b;
+
+            a = b;
+            b = c;
+            c = a+b ;
+
+            
+        } else {
+
+            result = a + b;
+
+        }
+
+      console.log(result);
+
+
+
+        
+    }
+
+    return result;
 
 };
+
+fibonacci(-5);
 
 // Do not edit below this line
 module.exports = fibonacci;
